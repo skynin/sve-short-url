66b18aa7d63b035de81ac506056442659853f407
Skynin <sohay_ua@yahoo.com>
2024-04-28 14:27:21 +0300

    Initial Cloudflare deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,16 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapter-cloudflare
+		adapter: adapter({			
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+	  platformProxy: {
+		persist: './.svelte-kit/cloudflare'
+	  }
+		})
 	}
 };
-
 export default config;
