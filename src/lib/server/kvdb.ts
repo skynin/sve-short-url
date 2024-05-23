e7f0c7b2c6cef8bbd985b100fdc3f97c5e73c786
Skynin <sohay_ua@yahoo.com>
2024-05-23 15:58:29 +0300

    change radix in hashString

--- src/lib/server/kvdb.ts
+++ src/lib/server/kvdb.ts
@@ -1,14 +1,14 @@
 /* string-hash
 ( https://github.com/darkskyapp/string-hash )
 https://moro.neocities.org/javascript-hash-functions+ */
-function hashString (origStr: string) : string {
+function hashString (origStr: string, radix: number = 36) : string {
 
   if (!origStr) return '42'
 
   let hash: number = 5381,
       i: number    = origStr.length
-  const firstSym: string = (origStr.length ? origStr.charCodeAt(0) : 42).toString(16),
-    secondSym: string = (origStr.length > 1 ? origStr.charCodeAt(1) : 42).toString(16)
+  const firstSym: string = (origStr.length ? origStr.charCodeAt(0) : 42).toString(radix),
+    secondSym: string = (origStr.length > 1 ? origStr.charCodeAt(1) : 42).toString(radix)
   
   while(i>2) // we already included first and second symbols
     hash = (hash * 33) ^ origStr.charCodeAt(--i)
@@ -16,7 +16,7 @@ function hashString (origStr: string) : string {
   /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
    * integers. Since we want the results to be always positive, convert the
    * signed int to an unsigned by doing an unsigned bitshift. */
-  return firstSym + secondSym + (hash >>> 0).toString(16) + origStr.length.toString(16).slice(-4);
+  return firstSym + secondSym + (hash >>> 0).toString(radix) + origStr.length.toString(radix).slice(-4);
 }
 
 export class KVwrapper {
